c1740b0644ae9bbfda25454b396817a44472033d
Thomas Phelps <60406674+felpsey@users.noreply.github.com>
2022-12-28 18:16:01 +0000

    Completed, arrow-functions

---
+++ javascript/data-structures/arrow-functions.js
@@ -0,0 +1,17 @@
+/**
+ * A function that takes two numbers as arguments and returns their sum.
+ * 
+ * @author Ethan Phelps
+ * @version 28.12.22
+ * @see https://edabit.com/challenge/QkvDge63crdGnMfvM
+ */
+
+/**
+ * Returns value of input
+ * @param {*} value
+ * @returns {*} Value of input
+ */
+arrowFunc = (value) =>
+{
+    return value
+}
