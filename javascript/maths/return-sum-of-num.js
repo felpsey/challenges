f499840130d56833103c3ff2f029123731ca0ba2
Thomas Phelps <60406674+felpsey@users.noreply.github.com>
2022-12-28 18:05:39 +0000

    Completed, return sum of num

---
+++ javascript/maths/return-sum-of-num.js
@@ -0,0 +1,18 @@
+/**
+ * A function that takes two numbers as arguments and returns their sum.
+ * 
+ * @author Ethan Phelps
+ * @version 28.12.22
+ * @see https://edabit.com/challenge/3LpBLgNRyaHMvNb4j
+ */
+
+/**
+ * Returns the sum of left and right
+ * @param {number} left
+ * @param {number} right
+ * @returns {number} Sum of left and right
+ */
+function addition(left, right)
+{
+    return left + right
+}
